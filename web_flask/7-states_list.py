e6b5711e53bb90a4b4d1291f902e1edcedbf66a3
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-22 21:06:38 +0200

    upadte files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
+"""You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+LI tag: description of one State: <state.id>: <B><state.name></B>"""
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+def close_session(exception):
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def display():
+    states = list(storage.all(State).values())
+    return render_template("7-states_list.html", states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port='5000')
